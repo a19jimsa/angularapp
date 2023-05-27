141edff25296c10a1ff59c0a4dd957eb13f48d4f
Jimmy Sandin <a19jimsa@student.his.se>
2023-05-27 21:43:44 +0200

    Changed styling

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, NgModel } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent {
-  log(x: NgModel) {
-    console.log(x);
+export class FormComponent implements OnInit {
+  myForm!: FormGroup;
+  //Dependency inject
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(10)]],
+      comment: ['', [Validators.required, Validators.minLength(10)]],
+    });
   }
+  get name() {
+    return this.myForm.get('name');
+  }
+
+  get comment() {
+    return this.myForm.get('comment');
+  }
+
+  add() {}
 }
