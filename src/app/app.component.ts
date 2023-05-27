303fa92edc180be483a930b3986cd3e65bdf7a04
Jimmy Sandin <a19jimsa@student.his.se>
2023-05-27 17:51:57 +0200

    Added formvalidation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Item } from './item';
 
 @Component({
@@ -6,18 +13,31 @@ import { Item } from './item';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   chess = false;
   title = 'todo';
 
   filter: 'all' | 'active' | 'done' = 'all';
 
-  allItems: Item[] = [
-    { description: 'eat', done: true },
-    { description: 'sleep', done: false },
-    { description: 'play', done: false },
-    { description: 'laugh', done: false },
-  ];
+  allItems: Item[] = [];
+  myForm!: FormGroup;
+
+  //Dependency inject
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      todo: ['', [Validators.required, Validators.minLength(5)]],
+    });
+    let items = localStorage.getItem('items');
+    if (items !== null) {
+      this.allItems = JSON.parse(items);
+    }
+  }
+
+  get todo() {
+    return this.myForm.get('todo');
+  }
 
   get items() {
     if (this.filter === 'all') {
@@ -30,19 +50,27 @@ export class AppComponent {
 
   add(description: string) {
     this.allItems.unshift({ description, done: false });
+    this.save();
   }
 
   delete(i: number) {
     this.allItems.splice(i, 1);
+    this.save();
   }
 
   change(item: Item) {
     this.remove(item);
     this.allItems.unshift(item);
+    this.save();
   }
 
   remove(item: Item) {
     this.allItems.splice(this.allItems.indexOf(item), 1);
+    this.save();
+  }
+
+  save() {
+    localStorage.setItem('items', JSON.stringify(this.allItems));
   }
 
   changeNav() {
