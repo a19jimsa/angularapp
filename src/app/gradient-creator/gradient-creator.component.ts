158ca2246f78c3e66922e3a4fd76e17dcd51d132
Jimmy Sandin <a19jimsa@student.his.se>
2026-07-30 23:58:44 +0200

    Fixed gradient textures, just positions bug left

--- src/app/gradient-creator/gradient-creator.component.ts
+++ src/app/gradient-creator/gradient-creator.component.ts
@@ -1,16 +1,28 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { CdkDrag, CdkDragEnd } from '@angular/cdk/drag-drop';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TextureManager } from 'src/resource-manager/texture-manager';
 
 type ColorStop = {
   color: string;
   stop: number;
+  position: Position;
+};
+
+type Position = {
+  x: number;
+  y: number;
 };
 
 @Component({
   selector: 'app-gradient-creator',
-  imports: [CdkDrag, ReactiveFormsModule],
+  imports: [CdkDrag, ReactiveFormsModule, FormsModule],
   templateUrl: './gradient-creator.component.html',
   styleUrl: './gradient-creator.component.css',
 })
@@ -21,13 +33,15 @@ export class GradientCreatorComponent {
   ctx!: CanvasRenderingContext2D;
   gradient!: CanvasGradient;
   colorStops: ColorStop[] = new Array();
-  position = { x: 0, y: 0 };
+  position: Position = { x: 0, y: 0 };
   color = new FormControl('');
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
   ngAfterViewInit() {
-    this.createCanvas();
     this.addColorStop(0, 'white');
     this.addColorStop(1, 'black');
+    this.createCanvas();
   }
 
   createCanvas() {
@@ -43,16 +57,22 @@ export class GradientCreatorComponent {
 
     for (let i = 0; i < this.colorStops.length; i++) {
       const colorStop = this.colorStops[i];
-      this.gradient.addColorStop(colorStop.stop, colorStop.color);
+      this.gradient.addColorStop(Math.abs(colorStop.stop), colorStop.color);
     }
 
     ctx.fillStyle = this.gradient;
     ctx.fillRect(0, 0, 256, 1);
+    this.cdr.detectChanges();
+    this.updateTexture();
   }
 
   addColorStop(time: number, color: string) {
-    this.colorStops.push({ color: color, stop: time });
-    this.createCanvas();
+    this.colorStops.push({
+      color: color,
+      stop: time,
+      position: { x: time, y: 0 },
+    });
+    this.cdr.detectChanges();
   }
 
   onDropped(event: CdkDragEnd, colorStop: ColorStop, color: string | null) {
@@ -65,7 +85,6 @@ export class GradientCreatorComponent {
       colorStop.color = color;
     }
     this.createCanvas();
-    this.updateTexture();
   }
 
   updateTexture() {
